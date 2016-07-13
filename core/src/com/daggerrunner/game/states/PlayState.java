2c87907a980c75ec306e6a6b699247209bbba770
abbasCoder51 <abbascoder51@gmail.com>
2016-07-13 20:23:38 +0100

    - Instance of Gem class created, - Remove instance from list when gem collides with player, - Render gem to screen, - Dispose of gem instance when game state changed

--- core/src/com/daggerrunner/game/states/PlayState.java
+++ core/src/com/daggerrunner/game/states/PlayState.java
@@ -2,15 +2,13 @@ package com.daggerrunner.game.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.daggerrunner.game.DaggerJumper;
 import com.daggerrunner.game.sprites.Enemy;
+import com.daggerrunner.game.sprites.Gem;
 import com.daggerrunner.game.sprites.Player;
-
-import sun.rmi.runtime.Log;
 
 public class PlayState extends State{
     private static final int ENEMY_SPACING = 150;
@@ -21,6 +19,8 @@ public class PlayState extends State{
     private Player player;
     private Texture bg;
     private Array<Enemy> enemies;
+    private Array<Gem> gems;
+//    private Gem gem;
     private Vector3 playerVec;
     private int playerCoordsXRight;
 
@@ -29,28 +29,45 @@ public class PlayState extends State{
         player = new Player(100, PLAYER_GROUND);
         playerCoordsXRight = (player.getAnimation().getWidth()/4);
         bg = new Texture("bg.png");
+
+        // set positioning of screen
         cam.setToOrtho(false, DaggerJumper.WIDTH/2, DaggerJumper.HEIGHT/2);
 
         playerVec = new Vector3(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),0);
         cam.unproject(playerVec);
 
-        enemies = new Array<Enemy>();
+//        gems.add(new Gem());
+        gems = new Array<Gem>();
+        for(int i = 1; i<=100;i++){
+            gems.add(new Gem());
 
-        for(int i = 1;i<=ENEMY_COUNT;i++)
-        {
-            enemies.add(new Enemy(i*(ENEMY_SPACING + Enemy.ENEMY_WIDTH)));
         }
+
+//        for(int i = 1;i<=10;i++)
+//        {
+//            gems.add(new Gem());
+//        }
+
+//        enemies = new Array<Enemy>();
+//
+//        for(int i = 1;i<=ENEMY_COUNT;i++)
+//        {
+//            enemies.add(new Enemy(i*(ENEMY_SPACING + Enemy.ENEMY_WIDTH)));
+//        }
+
     }
 
     @Override
     protected void handleInput() {
         if(Gdx.input.justTouched())
         {
+            System.out.print("Get X value: " + Gdx.input.getX());
+
             if((player.getPosition().y == PLAYER_GROUND) || (player.jumpCount() < 2))
             {
                 if((Gdx.graphics.getHeight()/2) > Gdx.input.getY())
                 {
-                    player.jump();
+//                    player.jump();
                 }
             }
 
@@ -59,11 +76,11 @@ public class PlayState extends State{
             cam.unproject(playerVec);
 
             if(player.getPosition().x < (playerVec.x - playerCoordsXRight)){
-                System.out.println("Move to right");
-                player.move(true, (int) (playerVec.x - playerCoordsXRight), "right");
+                player.move(true, (int) (playerVec.x - playerCoordsXRight), "right", true);
+                player.directionChanged(true);
             }else{
-                System.out.println("Move to left");
-                player.move(true, (int) (playerVec.x - playerCoordsXRight), "left");
+                player.move(true, (int) (playerVec.x - playerCoordsXRight), "left", true);
+                player.directionChanged(true);
             }
         }
     }
@@ -72,26 +89,43 @@ public class PlayState extends State{
     public void update(float dt) {
         handleInput();
         player.update(dt);
+
+
 //        cam.position.x = player.getPosition().x + 80;
 
-        if((player.getPosition().x > (playerVec.x - playerCoordsXRight)) && (player.movePlayer == true) && (player.direction == "right"))
-        {
-            player.move(false, 0, "right");
+        // stop character position of touched area on screen - right
+        if ((player.getPosition().x > (playerVec.x - playerCoordsXRight)) && (player.movePlayer == true) && (player.direction == "right")) {
+            player.move(false, 0, "right", false);
         }
 
-        if((playerVec.x > player.getPosition().x) && (player.movePlayer == true) && (player.direction == "left"))
-        {
-            player.move(false, 0, "left");
+        // stop character position of touched area on screen - right
+        if ((playerVec.x > player.getPosition().x) && (player.movePlayer == true) && (player.direction == "left")) {
+            player.move(false, 0, "left", false);
         }
 
+        for(int i=0;i<gems.size;i++){
+            Gem gem = gems.get(i);
+            gem.update(dt);
+
+            gem.resposition(gem.getPosition().x, gem.getPosition().y);
+
+            // check if gem collides with player or gem moves out of screen
+            if(gem.collides(player.getBounds()) || gem.getPosition().x <= 0){
+
+                // remove gem instance from list of array
+                gems.removeIndex(i);
+            }
+
+            Gdx.app.log("Gem", "Nice Gem #" + i);
+        }
 //        if(player.movePlayer)
 //        {
 //            if(player.getPosition().x > player.movePlayerX)
 //            {
-//                player.move(false, 0);
+//                player.move(false, 0, "");
 //            }
 //        }
-//        System.out.println("Current Position: " + player.getPosition());
+
 //        for(int i=0; i < enemies.size; i++)
 //        {
 //            Enemy enemy = enemies.get(i);
@@ -105,6 +139,25 @@ public class PlayState extends State{
 ////                gsm.set(new PlayState(gsm));
 //            }
 //        }
+
+//        for (int i = 0; i < gems.size; i++) {
+//            Gem gem = gems.get(i);
+//            gem.update(dt);
+//
+//            if(gem.collides(player.getBounds()))
+//            {
+////                gsm.set(new PlayState(gsm));
+//                gem.dispose();
+//            }
+//        }
+
+//        gem.resposition(gem.getPosition().x, gem.getPosition().y);
+//
+//        if(gem.collides(player.getBounds())){
+//            gem.dispose();
+//
+//        }
+
         cam.update();
 
     }
@@ -115,10 +168,23 @@ public class PlayState extends State{
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(player.getTexture(), player.getPosition().x, player.getPosition().y);
+//        if(!gem.collides(player.getBounds())) {
+//            sb.draw(gem.getTexture(), gem.getPosition().x, gem.getPosition().y);
+//        }
+
+        for(Gem gem : gems)
+        {
+            sb.draw(gem.getTexture(), gem.getPosition().x, gem.getPosition().y);
+        }
+
+//        for(Gem gem : gems){
+//            sb.draw(gem.getTexture(), gem.getPosition().x, gem.getPosition().y);
+//        }
 //        for(Enemy enemy : enemies)
 //        {
 //            sb.draw(enemy.getEnemy(), enemy.getPosEnemy().x, enemy.getPosEnemy().y);
 //        }
+//        sb.draw(gem.getGem(), gem.getPosGem().x, gem.getPosGem().y);
         sb.end();
     }
 
@@ -126,6 +192,10 @@ public class PlayState extends State{
     public void dispose() {
         bg.dispose();
         player.dispose();
+
+        for(Gem gem : gems) {
+            gem.dispose();
+        }
 //        for(Enemy enemy : enemies)
 //        {
 //            enemy.dispose();
